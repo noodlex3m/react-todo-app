ce777d9748e5a673c772b81f4eb92d7fc1d8042d
Sergii <noodlex3m@gmail.com>
2025-09-29 23:04:51 +0300

    start learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TodoList from "./components/TodoList.jsx";
+import AddTodoForm from "./components/AddTodoForm.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Вивчити основи React", isCompleted: true },
+    { id: 2, text: "Створити To-Do застосунок", isCompleted: false },
+    { id: 3, text: "Підкорити світ", isCompleted: false },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Мій список завдань</h1>
+      <AddTodoForm />
+      <TodoList todos={todos} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AddTodoForm.jsx
@@ -0,0 +1,10 @@
+function AddTodoForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Додайте нове завдання" />
+      <button type="submit">Додати</button>
+    </form>
+  );
+}
+
+export default AddTodoForm;
